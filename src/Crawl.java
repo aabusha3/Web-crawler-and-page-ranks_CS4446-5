6d3fa7169a7425c6b9461666981ed3e1f8be4609
aabusha3 <aabusha3@uwo.ca>
2023-12-05 00:34:59 -0500

    code implementated as per provided output

--- src/Crawl.java
+++ src/Crawl.java
@@ -10,42 +10,49 @@ public class Crawl {
    private static String baseURL = "https://www.csd.uwo.ca/faculty/solis/cs9668/test/";
    
    public static void main (String[] args) {
-       URL theUrl, u;
-       ArrayList<URL> foundURLS = new ArrayList<>();
-       HashSet<String> uniQue = new HashSet<>();
+       URL theUrl;
+       HashSet<String> foundURLS = new HashSet<>(), checkedURLS = new HashSet<>(), difURLS = new HashSet<>(), uniQue = new HashSet<>();
+       HashSet<URL>printURLS = new HashSet<>();
        BufferedReader input;
-       String s;
+       String s, u, theMod;
        String[] query;   // User specified query
-       boolean inFile;   // Set tru if any of the query words is in the document
-       int counter = 0;  // Counter for the number of hyperlinks
 
        try {
-           theUrl = new URL(baseURL+"test.html");  // This is the complete URL of the test page
+           foundURLS.clear();
+           checkedURLS.clear();
+           foundURLS.add("test.html");
            query = InOut.readQuery();
-           System.out.println(Arrays.toString(query));
 
-           input = new BufferedReader(new InputStreamReader(theUrl.openStream())); // Open URL for reading
-           inFile = false;
+           while (foundURLS.size() > checkedURLS.size()) {//loop through all new docs
+               uniQue.clear();
+               difURLS.addAll(foundURLS);
+               difURLS.removeAll(checkedURLS);
 
-           while ((s = input.readLine()) != null) {  // Read the document specified by theUrl
-               u = extractURL(s);
-               if (u != null) {
-                   foundURLS.add(u);
-                   ++counter;
+               theMod = difURLS.stream().findFirst().get();
+               theUrl = new URL(baseURL + theMod);
+               checkedURLS.add(theMod);
+
+               input = new BufferedReader(new InputStreamReader(theUrl.openStream()));
+
+               while ((s = input.readLine()) != null) { //read current doc
+                   u = extractURL(s);
+                   if (u != null) foundURLS.add(u);
+
+                   for (String value : query) {
+                       if ((s.toLowerCase()).contains(value.toLowerCase()))
+                           uniQue.add(value.toLowerCase());
+
+                       if (uniQue.size() == query.length)
+                           printURLS.add(theUrl);
+                   }
                }
-               // Check if any of the query words is in this line of the document
-               for (String value : query) {
-                   if ((s.toLowerCase()).contains(value.toLowerCase()))
-                       uniQue.add(value.toLowerCase());
 
-                   if (uniQue.size() == query.length)
-                       inFile = true;
-               }
+               input.close();
            }
-           if (inFile) InOut.printFileName(theUrl); // You MUST use this method to print an URL
-           input.close();
-           InOut.endListFiles(); // You MUST invoke this method before your program terminates
-           System.out.println("Number of hyperlinks found in the page: "+counter);
+           for (URL e : printURLS) InOut.printFileName(e);
+           InOut.endListFiles();
+           System.out.println("Number of documents: " + (foundURLS.size()-1));
+           System.out.println("\n----------------------------------------------------------------------------");
 
        } catch (MalformedURLException mue) {
            System.out.println("Malformed URL");
@@ -56,14 +63,14 @@ public class Crawl {
    
    /* If there is an URL embedded in the text passed as parameter, the URL will be extracted and
       returned; if there is no URL in the text, the value null will be returned              */   
-   public static URL extractURL(String text) throws MalformedURLException {
-   	String textUrl;
-      	int index = text.lastIndexOf("a href=");
-   	if (index > -1) {
-   		textUrl = baseURL+text.substring(index+8,text.length()-2);   // Form the complete URL	
-   		return new URL(textUrl);
-   	}
-   	else return null;
+   public static String extractURL(String text) {
+       String textUrl;
+       int index = text.lastIndexOf("a href=");
+       if (index > -1) {
+           textUrl = text.substring(index+8,text.length()-2);
+           return textUrl;
+       }
+       else return null;
    }
 } 
 
